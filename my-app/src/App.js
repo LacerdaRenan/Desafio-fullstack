60bc073dc6ca9dd4dffd9666f7fdac16b74db9f2
RenanLacerda <renanrodlacerda@gmail.com>
2022-05-28 18:05:07 -0300

    creating pag new estoque, new produto

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Home from './components/Home';
 import AdminProdutos from './components/admin/AdminProdutos';
 import AdminEstoques from './components/admin/AdminEstoques';
+import AdminProdutoNovo from './components/admin/AdminProdutoNovo';
+import AdminEstoqueNovo from './components/admin/AdminEstoqueNovo';
 
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
@@ -13,6 +15,8 @@ function App() {
           <Route exact path='/' element={<Home/>}></Route>
           <Route path='/admin/produtos' element={<AdminProdutos/>}></Route>
           <Route path='/admin/estoques' element={<AdminEstoques/>}></Route>
+          <Route path='/admin/produtos/novo' element={<AdminProdutoNovo/>}></Route>
+          <Route path='/admin/estoques/novo' element={<AdminEstoqueNovo/>}></Route>
         </Routes>
       </Router>
     </div>
---
+++ my-app/src/components/admin/AdminEstoqueNovo.jsx
@@ -0,0 +1,61 @@
+import NavBarAdmin from "./NavBarAdmin";
+import axios from "axios";
+import React, {useState, useEffect} from "react";
+
+function AdminEstoqueNovo(){
+
+    function criarEstoque(e){
+        e.preventDefault();
+
+        let estoque = {
+            produtoId: e.target.produto.value,
+            valor: e.target.quantidade.value
+        }
+
+        axios.post('http://localhost:5000/estoque',estoque)
+            .then(()=>{e.target.submit()})
+            .catch(err=>{alert('Não foi possível concluir o cadastro, já existe estoque cadastrado para esse produto!')})
+        //
+    }
+
+    let[posts, setPosts] = useState([]);
+ 
+    useEffect(()=>{
+        axios.get('http://localhost:5000/produto')
+        .then(data=>{setPosts(data.data)})
+        .catch(err=>{console.log(err)})
+    }, [])
+
+    return(
+        <>
+            <NavBarAdmin/>
+            <div className="container">
+                <hr />
+                <br />
+                <div className="card">
+                    <div className="card-header">
+                        <h2>Cadastro de Estoque</h2>
+                    </div>
+                    <div className="card-body">
+                        <form onSubmit={criarEstoque} action="/admin/estoques">
+                            <label>Escolha o Produto</label>
+                            <br />
+                            <select name="produto" className="form-control">
+                                {posts.map(produto=>(
+                                    <option key={produto.id} value={produto.id}>{produto.nome}</option>
+                                ))}
+                            </select>
+                            <br />
+                            <label htmlFor="Valor">Valor</label>
+                            <input placeholder="Quantidade em Estoque" className="form-control" type="number" name="quantidade" id="valor"/>
+                            <br />
+                            <button className="btn btn-success">Cadastrar Estoque</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default AdminEstoqueNovo;
